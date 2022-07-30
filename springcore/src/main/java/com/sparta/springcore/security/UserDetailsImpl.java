b906573bf3a21454a4ed92e2f974418d792d1961
Jungmin-Noh <lumill925@gmail.com>
2022-07-30 16:51:08 +0900

    강의 2주차

---
+++ springcore/src/main/java/com/sparta/springcore/security/UserDetailsImpl.java
@@ -0,0 +1,65 @@
+package com.sparta.springcore.security;
+
+import com.sparta.springcore.model.UserRoleEnum;
+import com.sparta.springcore.model.Users;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final Users users;
+
+    public UserDetailsImpl(Users users) {
+        this.users = users;
+    }
+
+    public Users getUser() {
+        return users;
+    }
+
+    @Override
+    public String getPassword() {
+        return users.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum userRole = users.getRole();
+        String authority = userRole.getAuthority();
+
+        SimpleGrantedAuthority simpleAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleAuthority);
+
+        return authorities;
+    }
+}
